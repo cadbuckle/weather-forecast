8bbd93a9e39afc8361e81dfe65ae944df3ef9d9e
Duncan B <duncanbuckle@gmail.com>
2024-01-25 15:12:38 +0000

    Saves entered item to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,160 +2,202 @@
 // 1 When page is opened
 //      Have Input box for city/town/location
 //      Search button to find weather for entered location
-//      Large box to right to display current weather (as per "2")
-//      5 day forecase for location display under large box
-//      "History" (beneath search button) is populated with saved list of searched cities 
+//      "History" (beneath search button) is populated with saved list of searched cities
 //      List is clickable per "5" below
-// 2 When a user searches for a city they are 
-//      presented with current conditions for that city
-//      presented with future conditions for that city
+// 2 When a user searches for a city they are
 //      city is added to the search history (localstorage)
-// 3 When a user views the current weather conditions for that city they are presented with:
-//      This is populated in to the "today" section
-//      The city name
-//      The date
-//      An icon representation of weather conditions
-//      The temperature
-//      The humidity
-//      The wind speed
-// 4 When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
-//      This is populated in to the "forecast" section as cards
-//      The date
-//      An icon representation of weather conditions
-//      The temperature
-//      The humidity
-// 5 When a user click on a city in the search history they are 
+// 5 When a user click on a city in the search history they are
 //      presented with current conditions for that city
 //      presented with future conditions for that city
+
+// define constants
+const apiKey = "c69b59d2c54d81b754f2d4320cd54364";
+const iconExt = ".png"; // 1x icon size
+// const iconExt = "@2x.png";  // 2x icon size
+
+// define element variables
+var searchBtnEl = document.querySelector("#search-button");
+var searchTextEl = document.querySelector("#search-input");
 
 // define variables
-var searchBtnEl=document.querySelector("#search-button");
 var weatherData;
-var apiKey = "c69b59d2c54d81b754f2d4320cd54364";
 var currentDate;
+var historyArray = ["London", "New York", "Paris", "Munich"];
+var lcHistoryArray = ["london", "new york", "paris", "munich"];
 
-function buildHeader(){
-    // Build header
-    var todayEl = $("#today");
-    todayEl.empty();
-    todayEl.attr("class", "row mt-3 card")
-    todayEl.append($("<h2>").text("London ("+dayjs().format("DD/MM/YYYY")+")"));
+function buildHeader() {
+  // Build header
+  cityEntered = weatherData.city.name
+  var todayEl = $("#today");
+  todayEl.empty();
+  todayEl.attr("class", "row mt-3 bigcard card");
+  todayEl.append(
+    $("<h2>").text(
+      cityEntered + " (" + dayjs().format("DD/MM/YYYY") + ")"
+    )
+  );
 
-    // add weather image
-    var headerEl = $("<p>");
+  // add weather image
+  var headerEl = $("<p>");
 
-    // add Temp, Wind and Humidty
-    // list[0] is now!
-    headerEl.append($("<img>").attr("src", "https://openweathermap.org/img/wn/"+ weatherData.list[0].weather[0].icon+"@2x.png"));
-    headerEl.append($("<div>").text("Temp: "+weatherData.list[0].main.temp));
-    headerEl.append($("<div>").text("Wind: "+weatherData.list[0].wind.speed));
-    headerEl.append($("<div>").text("Humidity: "+weatherData.list[0].main.humidity+"%"));
-    todayEl.append(headerEl);
-    currentDate = dayjs.unix(weatherData.list[0].dt).format("DD/MM/YYYY")
+  // add Temp, Wind and Humidty
+  // list[0] is now!
+  headerEl.append(
+    $("<img>").attr(
+      "src",
+      "https://openweathermap.org/img/wn/" +
+        weatherData.list[0].weather[0].icon +
+        iconExt
+    )
+  );
+  headerEl.append($("<div>").text("Temp: " + weatherData.list[0].main.temp));
+  headerEl.append($("<div>").text("Wind: " + weatherData.list[0].wind.speed));
+  headerEl.append(
+    $("<div>").text("Humidity: " + weatherData.list[0].main.humidity + "%")
+  );
+  todayEl.append(headerEl);
+  currentDate = dayjs.unix(weatherData.list[0].dt).format("DD/MM/YYYY");
 }
-function buildForecast(){
+function buildForecast() {
+  var forecastData = weatherData.list;
 
-    console.log("build 5 day forecast", weatherData);
+  var forecastEl = $("#forecast");
+  forecastEl.empty();
+  forecastEl.append($("<h3>").text("5-Day Forecast:"));
 
-    var forecastData=weatherData.list;
-    
-    var forecastEl = $("#forecast");
-    forecastEl.empty();
-    forecastEl.append($("<h3>").text("5-Day Forecast:"));
+  // count for number of days
+  var day = 0;
 
-    // count for number of days
-    var day=0;
-
-    // iterate through data and build new card on change of date
-    for (let i = 1; i <= 70; i++){
-
-        if (currentDate != dayjs.unix(forecastData[i].dt).format("DD/MM/YYYY")){
-            day++;
-            currentDate = dayjs.unix(forecastData[i].dt).format("DD/MM/YYYY");
-            var nc = newForecastCard(
-                currentDate,
-                forecastData[i].main.temp,
-                forecastData[i].wind.speed,
-                forecastData[i].main.humidity,
-                forecastData[i].weather[0].icon
-                );
-            forecastEl.append(nc);
-            if (day > 5){
-                break;
-            }
-        }
+  // iterate through data and build new card on change of date
+  for (let i = 1; i < weatherData.cnt; i++) {
+    if (currentDate != dayjs.unix(forecastData[i].dt).format("DD/MM/YYYY")) {
+      day++;
+      currentDate = dayjs.unix(forecastData[i].dt).format("DD/MM/YYYY");
+      var nc = newForecastCard(
+        currentDate,
+        forecastData[i].main.temp,
+        forecastData[i].wind.speed,
+        forecastData[i].main.humidity,
+        forecastData[i].weather[0].icon
+      );
+      forecastEl.append(nc);
+      if (day > 5) {
+        break;
+      }
     }
-};
+  }
+}
 
-function newForecastCard(cardDate, cardTemp, cardWind, cardHumdity, cardIcon){
+function newForecastCard(cardDate, cardTemp, cardWind, cardHumdity, cardIcon) {
+  // define card
+  const newCard = document.createElement("div");
+  newCard.classList.add("col-2");
+  newCard.classList.add("card");
 
-    // define card
-    const newCard = document.createElement('div');
-    newCard.classList.add('col-2');
-    newCard.classList.add('card');
+  // define card body
+  const newCardBody = document.createElement("div");
+  newCardBody.classList.add("col-2");
+  newCardBody.classList.add("card");
+  // newCardBody.classList.add('card-body');
 
-    // define card body
-    const newCardBody = document.createElement('div');
-    newCardBody.classList.add('card-body');
+  // define card title
+  const newCardTitle = document.createElement("h5");
+  newCardTitle.classList.add("card-title");
+  newCardTitle.textContent = cardDate;
+  newCardBody.appendChild(newCardTitle);
 
+  // define weather image
+  const newCardImg = document.createElement("img");
+  newCardImg.src = "https://openweathermap.org/img/wn/" + cardIcon + iconExt;
+  newCardBody.appendChild(newCardImg);
 
-    // define card title
-    const newCardTitle = document.createElement('h5');
-    newCardTitle.classList.add('card-title');
-    newCardTitle.textContent = cardDate;
-    newCardBody.appendChild(newCardTitle);
+  // define card temp
+  const newCardTemp = document.createElement("ul");
+  newCardTemp.textContent = "Temp: " + cardTemp;
+  newCardBody.appendChild(newCardTemp);
 
-    // define weather image
-    const newCardImg = document.createElement('img');
-    newCardImg.src = "https://openweathermap.org/img/wn/"+cardIcon+"@2x.png";
-    newCardBody.appendChild(newCardImg);
+  // define card wind
+  const newCardWind = document.createElement("ul");
+  newCardWind.textContent = "Wind: " + cardWind;
+  newCardBody.appendChild(newCardWind);
 
-    // define card temp
-    const newCardTemp = document.createElement('ul');
-    newCardTemp.textContent = "Temp: "+cardTemp;
-    newCardBody.appendChild(newCardTemp);
-
-    // define card wind
-    const newCardWind = document.createElement('ul');
-    newCardWind.textContent = "Wind: "+cardWind;
-    newCardBody.appendChild(newCardWind);
-
-    // define card humidity
-    const newCardHumid = document.createElement('ul');
-    newCardHumid.textContent = "Humidity: "+cardHumdity;
-    newCardBody.appendChild(newCardHumid);
-    newCard.appendChild(newCardBody);
-
-    return newCard;
+  // define card humidity
+  const newCardHumid = document.createElement("ul");
+  newCardHumid.textContent = "Humidity: " + cardHumdity;
+  newCardBody.appendChild(newCardHumid);
+  newCard.appendChild(newCardBody);
+  return newCardBody;
+  // return newCard;
 }
 
 // "Search" for entered location on API and return weather
 searchBtnEl.addEventListener("click", searchWeather);
 
-// function called from search button
-async function searchWeather(event){
-    event.preventDefault();
-    var cityLat = "51.5072";
-    var cityLon = "0.1276";
-    var city = "London,uk";
+async function searchWeather(event) {
+  event.preventDefault();
+
+  // get entered city
+  var cityLat = "51.5072";
+  var cityLon = "0.1276";
+  var cityEntered = searchTextEl.value;
+
+  if (!cityEntered) {
+    alert("Please enter a location for the forecast");
+  } else {
     var weatherResponse;
 
     // 5 day forecast for location
-    apiCall="http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey+"&units=metric";
+    apiCall =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityEntered +
+      "&appid=" +
+      apiKey +
+      "&units=metric";
     weatherResponse = await fetch(apiCall);
     weatherData = await weatherResponse.json();
-    console.log(apiCall);
-
-    // fetch(apiCall)
-    //     .then(function (response) {
-    //   weatherResponse = response.json();
-    //   console.log(weatherResponse);
-    //   return weatherResponse;
-    // })
-    // .then(function (weatherData) {
-    //   return weatherData.list;
-    // });
 
     buildHeader();
     buildForecast();
-};
+
+    saveToHistory();
+  }
+}
+
+function saveToHistory() {
+  console.log("Save");
+  var lcCity = cityEntered.toLowerCase();
+  // if city not in array then add to array and redisplay the history
+  if (!lcHistoryArray.includes(lcCity)) {
+    lcHistoryArray.push(lcCity);
+    historyArray.push(cityEntered);
+    displayHistory();
+  };
+}
+
+function displayHistory() {
+  console.log("Draw History");
+  
+  $("#history").empty();
+  for (let i = 0; i < historyArray.length; i++) {
+    const arrayItem = historyArray[i];
+    // define button
+    var a = $("<button>");
+    // assign data-name as the location
+    a.attr("data-name",historyArray[i]);
+    // assign style to button
+    a.addClass("btn history-button border-curve");
+    // add button text
+    a.text(historyArray[i])
+    // add button to list
+    $("#history").append(a);
+  }
+
+}
+
+function loadFromHistory() {
+  console.log("Load");
+
+  displayHistory();
+}
+
+loadFromHistory();
