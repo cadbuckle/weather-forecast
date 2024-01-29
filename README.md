f59c197008ea58639f360888eb55993af1cfb21b
Duncan B <duncanbuckle@gmail.com>
2024-01-29 13:14:03 +0000

    Updated ReadMe. Added screenshots.

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# weather-forecast
+# Work Day Schedule
+
+## Description
+This app allows a user to view the current (and future) weather for an entered location.
+
+## Table of Contents
+* [Links](#links)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Screenshots](#screenshots)
+* [License](#license)
+* [Features](#features)
+
+## Links
+- [Repo](https://github.com/cadbuckle/weather-forecast)
+- [Live](https://cadbuckle.github.io/weather-forecast/)
+
+## Installation
+Saved search history is held locally in the browser in a key called "wk08-weather-history".
+
+## Usage
+Upon first run, the screen will prompt for a location whose weather will then be displayed.
+
+Each location searched for will be added at the top of the search history. If the location is already in the list, it will not be re-added. A maximum of 6 locations will be stored. If the list already contains 6 locations, the locatoin at the bottom of the list will be removed.
+
+Each entry in the search history can be "searched again" simply by clicking the location in the list.
+
+## Screenshots
+### Screenshot 1 - No locations searched
+<img src="./assets/images/screenshot1.png" width=75% alt="screenshot 1"></img>
+### Screenshot 2 - Searched item
+<img alt="screenshot 2" src="./assets/images/screenshot2.png" width=75%></img>
+### Screenshot 3 - Showing previously searched locations
+<img alt="screenshot 3" src="./assets/images/screenshot3.png" width=75%></img>
+
+## License
+MIT License as per repository.
+
+## Features
+JavaScript, notably:
+- API query to OpenWeatherMap
+- Display of returned current and forecast weather data
+- Local Storage: for recording the searched locations
+
+## Acknowledgements
+Thanks to:
+* Bootcamp Lessons
+* OpenWeatherMap.org
+
+© 2024 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@
 const apiKey = "c69b59d2c54d81b754f2d4320cd54364";
 const iconExt = ".png"; // 1x icon size
 // const iconExt = "@2x.png";  // 2x icon size
+const degree = "°C";
 
 // define element variables
 var searchBtnEl = document.querySelector("#search-button");
@@ -23,43 +24,44 @@ var searchTextEl = document.querySelector("#search-input");
 var weatherData;
 var currentDate;
 var cityEntered;
-var apicall;
+var apiCall;
 var historyArray = [];
 var lcHistoryArray = [];
 
 function buildHeader() {
   // Build header
   cityEntered = weatherData.city.name;
+  var weatherNow = weatherData.list[0];
   var todayEl = $("#today");
   todayEl.empty();
   todayEl.attr("class", "row mt-3 bigcard card");
 
-  var cityDateIcon =
-    "<h2>" +
-    cityEntered +
-    " (" +
-    dayjs().format("DD/MM/YYYY") +
-    ") " +
-    "<img src='https://openweathermap.org/img/wn/" +
-    weatherData.list[0].weather[0].icon +
-    iconExt +
-    "'</img>" +
-    "</h2>";
-  todayEl.append(cityDateIcon);
+  // div, p (styled as h2), img
+  var divEl = $("<div>");
+  divEl.addClass("currentWeather");
+  var pEl = $("<p>");
+  pEl = cityEntered+    " (" +  dayjs().format("DD/MM/YYYY") + ") ";
+  var imgEl = $("<img>");
+  imgEl.attr("src",'https://openweathermap.org/img/wn/'+
+  weatherNow.weather[0].icon +
+  iconExt);
 
+  divEl.append(pEl);
+  divEl.append(imgEl);
+  todayEl.append(divEl);
 
   // add weather image
   var headerEl = $("<p>");
 
   // add Temp, Wind and Humidty
   // list[0] is now!
-  headerEl.append($("<div>").text("Temp: " + weatherData.list[0].main.temp));
-  headerEl.append($("<div>").text("Wind: " + weatherData.list[0].wind.speed));
+  headerEl.append($("<div>").text("Temp: " + weatherNow.main.temp+degree));
+  headerEl.append($("<div>").text("Wind: " + weatherNow.wind.speed+"m/s"));
   headerEl.append(
-    $("<div>").text("Humidity: " + weatherData.list[0].main.humidity + "%")
+    $("<div>").text("Humidity: " + weatherNow.main.humidity + "%")
   );
   todayEl.append(headerEl);
-  currentDate = dayjs.unix(weatherData.list[0].dt).format("DD/MM/YYYY");
+  currentDate = dayjs.unix(weatherNow.dt).format("DD/MM/YYYY");
 }
 function buildForecast() {
   var forecastData = weatherData.list;
@@ -114,17 +116,17 @@ function newForecastCard(cardDate, cardTemp, cardWind, cardHumdity, cardIcon) {
   newCardBody.appendChild(newCardImg);
 
   // define card temp
-  const newCardTemp = document.createElement("ul");
-  newCardTemp.textContent = "Temp: " + cardTemp;
+  const newCardTemp = document.createElement("p");
+  newCardTemp.textContent = "Temp: " + cardTemp+degree;
   newCardBody.appendChild(newCardTemp);
 
   // define card wind
-  const newCardWind = document.createElement("ul");
-  newCardWind.textContent = "Wind: " + cardWind;
+  const newCardWind = document.createElement("p");
+  newCardWind.textContent = "Wind: " + cardWind+"m/s";
   newCardBody.appendChild(newCardWind);
 
   // define card humidity
-  const newCardHumid = document.createElement("ul");
+  const newCardHumid = document.createElement("p");
   newCardHumid.textContent = "Humidity: " + cardHumdity+"%";
   newCardBody.appendChild(newCardHumid);
   newCard.appendChild(newCardBody);
@@ -135,9 +137,8 @@ async function searchWeather(event) {
   event.preventDefault();
 
   // get entered city
-  var cityLat = "51.5072";
-  var cityLon = "0.1276";
   cityEntered = searchTextEl.value.trim();
+  searchTextEl.value = "";
 
   if (!cityEntered) {
     alert("Please enter a location for the forecast");
@@ -167,10 +168,18 @@ function saveToHistory() {
   var lcCity = cityEntered.toLowerCase();
   // if city not in array then add to array and redisplay the history
   if (!lcHistoryArray.includes(lcCity)) {
-    lcHistoryArray.push(lcCity);
-    historyArray.push(cityEntered);
+    // only retain 6 items in history
+
+    var lcHistArr5 = [];
+    lcHistArr5.push(lcCity);
+    var histArr5 = [];
+    histArr5.push(cityEntered);
+    var lcHArr = lcHistArr5.concat(lcHistoryArray.slice(0,5));
+    var hArr = histArr5.concat(historyArray.slice(0,5));
+    historyArray = hArr;
     // save list to local storage
-    localStorage.setItem("wk08-weather-history", JSON.stringify(historyArray));
+    localStorage.setItem("wk08-weather-history", JSON.stringify(hArr));
+
     // redraw history list
     displayHistory();
   }
